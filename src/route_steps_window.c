d38b80d226c104344e311543e02578a15fc9d81d
Remus Lazar <remus@lazar.info>
2014-01-22 11:32:34 +0100

    reindending w/o tabs

--- src/route_steps_window.c
+++ src/route_steps_window.c
@@ -20,47 +20,47 @@ static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t secti
 }
 
 static void route_step_cell_draw(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, const char *distance, const char *instructions) {
-	graphics_context_set_text_color(ctx, GColorBlack);
-	int y = 0;
-	if (cell_index->row > 0) {
-		graphics_draw_text(ctx,
-											 distance,
-											 fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD),  
-											 GRect(0,-2,144,24),
-											 GTextOverflowModeWordWrap,  
-											 GTextAlignmentLeft,  
-											 NULL);
-		y += 24;
-	}
+  graphics_context_set_text_color(ctx, GColorBlack);
+  int y = 0;
+  if (cell_index->row > 0) {
+    graphics_draw_text(ctx,
+		       distance,
+		       fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD),  
+		       GRect(0,-2,144,24),
+		       GTextOverflowModeWordWrap,  
+		       GTextAlignmentLeft,  
+		       NULL);
+    y += 24;
+  }
 	
-	graphics_draw_text(ctx,
-										 instructions,
-										 fonts_get_system_font(FONT_KEY_GOTHIC_24),  
-										 GRect(0,y,144,144),
-										 GTextOverflowModeWordWrap,  
-										 GTextAlignmentLeft,  
-										 NULL);
+  graphics_draw_text(ctx,
+		     instructions,
+		     fonts_get_system_font(FONT_KEY_GOTHIC_24),  
+		     GRect(0,y,144,144),
+		     GTextOverflowModeWordWrap,  
+		     GTextAlignmentLeft,  
+		     NULL);
 
-	if (cell_index->row == route_steps->current_step) {
-		graphics_draw_text(ctx,
-											 "*",
-											 fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD),
-											 GRect(100,-2,42,44),
-											 GTextOverflowModeWordWrap,
-											 GTextAlignmentRight,
-											 NULL);		
-	}
+  if (cell_index->row == route_steps->current_step) {
+    graphics_draw_text(ctx,
+		       "*",
+		       fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD),
+		       GRect(100,-2,42,44),
+		       GTextOverflowModeWordWrap,
+		       GTextAlignmentRight,
+		       NULL);		
+  }
 }
 
 static int16_t menu_get_cell_height_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
-	GSize size = graphics_text_layout_get_content_size(
-																										 route_steps->data[cell_index->row].instructions,
-																										 fonts_get_system_font(FONT_KEY_GOTHIC_24),
-																										 GRect(0,0,144,144),
-																										 GTextOverflowModeWordWrap,
-																										 GTextAlignmentLeft
-																										 );
-	return size.h + (cell_index->row == 0 ? 0 : 30);
+  GSize size = graphics_text_layout_get_content_size(
+						     route_steps->data[cell_index->row].instructions,
+						     fonts_get_system_font(FONT_KEY_GOTHIC_24),
+						     GRect(0,0,144,144),
+						     GTextOverflowModeWordWrap,
+						     GTextAlignmentLeft
+						     );
+  return size.h + (cell_index->row == 0 ? 0 : 30);
 }
 
 // This is the menu item draw callback where you specify what each item should look like
@@ -69,13 +69,13 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
   if (cell_index->row >= route_steps->count) return; // index out of bounds
 
   route_step_cell_draw(ctx,
-											 cell_layer,
-											 cell_index,
-											 cell_index->row == route_steps->current_step ?
-											 route_steps->current_distance  :
-											 route_steps->data[cell_index->row].distance,
-											 route_steps->data[cell_index->row].instructions
-											 );
+		       cell_layer,
+		       cell_index,
+		       cell_index->row == route_steps->current_step ?
+		       route_steps->current_distance  :
+		       route_steps->data[cell_index->row].distance,
+		       route_steps->data[cell_index->row].instructions
+		       );
 }
 
 void route_steps_window_reload_data(void) {
@@ -98,12 +98,12 @@ static void window_load(Window *window) {
   
   // Set all the callbacks for the menu layer
   menu_layer_set_callbacks(ui.detail_menu_layer, NULL, (MenuLayerCallbacks){
-    .get_num_sections = menu_get_num_sections_callback,
-    .get_num_rows = menu_get_num_rows_callback,
-    .get_cell_height = menu_get_cell_height_callback,
-    .draw_row = menu_draw_row_callback,
-    .select_click = menu_select_callback,
-  });
+      .get_num_sections = menu_get_num_sections_callback,
+	.get_num_rows = menu_get_num_rows_callback,
+	.get_cell_height = menu_get_cell_height_callback,
+	.draw_row = menu_draw_row_callback,
+	.select_click = menu_select_callback,
+	});
 
   // Bind the menu layer's click config provider to the window for interactivity
   menu_layer_set_click_config_onto_window(ui.detail_menu_layer, window);
@@ -143,6 +143,6 @@ bool route_steps_window_is_loaded() {
 }
 
 void route_steps_window_update(void) {
-	menu_layer_reload_data(ui.detail_menu_layer);
-	menu_layer_set_selected_index(ui.detail_menu_layer, (MenuIndex) { .row = route_steps->current_step, .section = 0 }, MenuRowAlignCenter, true);
+  menu_layer_reload_data(ui.detail_menu_layer);
+  menu_layer_set_selected_index(ui.detail_menu_layer, (MenuIndex) { .row = route_steps->current_step, .section = 0 }, MenuRowAlignCenter, true);
 }
