e9001282e637d4450004ca379a98aabdbba00448
Remus Lazar <remus@lazar.info>
2014-01-23 13:58:18 +0100

    waiting screen with the iphone symbol

--- src/window_waiting.c
+++ src/window_waiting.c
@@ -3,8 +3,9 @@
 
 static struct UI {
   Window *window;
-  TextLayer *text_layer;
-
+  /* TextLayer *text_layer; */
+  BitmapLayer *iphone_layer;
+  GBitmap *iphone_bmp;
   // Activity Layer
   Layer *activity_layer;
   uint8_t active_point;
@@ -35,23 +36,32 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   // Loading Text
-  ui.text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(ui.text_layer, "Waiting for data ..");
-  text_layer_set_text_alignment(ui.text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(ui.text_layer));
-  
+  /* ui.text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } }); */
+  /* text_layer_set_text(ui.text_layer, "Waiting for data .."); */
+  /* text_layer_set_text_alignment(ui.text_layer, GTextAlignmentCenter); */
+  /* layer_add_child(window_layer, text_layer_get_layer(ui.text_layer)); */
+
   // Activity Indicator
   ui.active_point = 0;
-  ui.activity_layer = layer_create((GRect) { .origin = { 42, 36 }, .size = { 62, 14 } });
+  ui.activity_layer = layer_create((GRect) { .origin = { 42, 110 }, .size = { 62, 14 } });
   layer_set_update_proc(ui.activity_layer, activity_layer_update_proc);
   layer_add_child(window_layer, ui.activity_layer);
+
+  // iPhone Symbol
+  ui.iphone_bmp = gbitmap_create_with_resource(RESOURCE_ID_IPHONE_ROUTE);
+  ui.iphone_layer = bitmap_layer_create((GRect) { .origin = { 0, 20 }, .size = { bounds.size.w, 74 } });
+  bitmap_layer_set_bitmap(ui.iphone_layer, ui.iphone_bmp);
+  bitmap_layer_set_alignment(ui.iphone_layer, GAlignCenter);
+  layer_add_child(window_layer, bitmap_layer_get_layer(ui.iphone_layer));
 
   tick_timer_service_subscribe(SECOND_UNIT, second_tick_handler);
 }
 
 static void window_unload(Window *window) {
   tick_timer_service_unsubscribe();
-  text_layer_destroy(ui.text_layer);
+  /* text_layer_destroy(ui.text_layer); */
+  bitmap_layer_destroy(ui.iphone_layer);
+  gbitmap_destroy(ui.iphone_bmp);
   layer_destroy(ui.activity_layer);
 }
 
